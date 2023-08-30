2fa8602a0ebb18ed13dd84d35fad7579e567ced6
Ivan Calderon <O788605@c0446ynhjq.naeast.ad.jpmorganchase.com>
2023-08-30 12:59:29 -0400

    Change link description

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Visit my site
+          ivancalderon.net
         </a>
       </header>
     </div>
